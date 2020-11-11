a19d0bf3e1b4716034afc709dda4dc637fccdf32
JML <Jlee3424@gmail.com>
2020-11-11 14:20:19 -0600

    Add files via upload

---
+++ src/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Dashboard() {
+
+  return (
+    <div>
+      <p>This is the second page.</p>
+    </div>
+  );
+
+
+ }
+
+ export default Dashboard;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Dashboard from './Dashboard';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+
+ReactDOM.render(
+  <React.StrictMode>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/Dashboard" />
+    </Switch>
+  </BrowserRouter>,
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
